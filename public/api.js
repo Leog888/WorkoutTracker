3a725f1f429e568cd68b3eadc1c84e951f9d0253
Leo <leonardodi.gutierrez@gmail.com>
2021-11-15 23:09:25 -0800

    will not export

---
+++ public/api.js
@@ -0,0 +1,48 @@
+
+   
+const API = {
+    // done
+    async getLastWorkout() {
+      let res;
+      try {
+        res = await fetch('/api/workouts');
+      } catch (err) {
+        console.log(err);
+      }
+      const json = await res.json();
+  
+      return json;
+    },
+    //done
+    async addExercise(data) {
+      const id = location.search.split('=')[1];
+  
+      const res = await fetch(`/api/workouts/${id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+      });
+  
+      const json = await res.json();
+  
+      return json;
+    },
+    // done
+    async createWorkout(data = {}) {
+      const res = await fetch('/api/workouts', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      const json = await res.json();
+  
+      return json;
+    },
+    async getWorkoutsInRange() {
+      const res = await fetch(`/api/workouts/range`);
+      const json = await res.json();
+  
+      return json;
+    },
+  };
